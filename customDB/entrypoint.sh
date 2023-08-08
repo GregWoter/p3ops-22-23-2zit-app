9bf4be30e62837aa3d5a9e3b48380c66f4622841
GregWoter <greg.woter@student.hogent.be>
2023-08-08 20:10:06 +0200

    edit sql dockerfile

--- customDB/entrypoint.sh
+++ customDB/entrypoint.sh
@@ -1,2 +1,2 @@
 # Run Microsoft SQl Server and initialization script (at the same time)
-/usr/src/app/run-initialization.sh & /opt/mssql/bin/sqlservr
+/usr/run-initialization.sh & /opt/mssql/bin/sqlservr
